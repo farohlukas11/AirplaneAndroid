ca18040e033e2dc486099caf02cdec262271ae33
Faroh Muhammad Lukas <farohlukas11@gmail.com>
2023-08-30 00:09:15 +0700

    menambahkan fungsi getUserById dan setUser serta signOut, selain itu juga menambahkan model UserJson.kt

--- app/src/main/java/com/faroh/airplaneandroid/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/faroh/airplaneandroid/core/data/source/remote/RemoteDataSource.kt
@@ -4,10 +4,12 @@ import android.util.Log
 import com.faroh.airplaneandroid.core.data.source.remote.response.ApiResponse
 import com.faroh.airplaneandroid.core.domain.model.SignInBody
 import com.faroh.airplaneandroid.core.domain.model.SignUpBody
+import com.faroh.airplaneandroid.core.domain.model.UserJson
 import com.faroh.airplaneandroid.core.domain.model.UserModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import io.reactivex.BackpressureStrategy
+import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.subjects.PublishSubject
 import javax.inject.Inject
@@ -56,7 +58,54 @@ class RemoteDataSource @Inject constructor(
                     )
                 } else {
                     result.onNext(ApiResponse.Error(it.exception?.message.toString()))
-                    Log.e("REMOTE SIGN UP", it.exception.toString())
+                    Log.e("REMOTE SIGN IN", it.exception.toString())
+                }
+            }
+        return result.toFlowable(BackpressureStrategy.BUFFER)
+    }
+
+    fun signOut(): Completable {
+        firebaseAuth.signOut()
+        return Completable.complete()
+    }
+
+    fun setUser(userModel: UserModel): Completable {
+        firebaseFirestore.collection("users").document(userModel.id!!).set({
+            UserJson(
+                email = userModel.email,
+                name = userModel.name,
+                hobby = userModel.hobby,
+                balance = userModel.balance
+            )
+        }).addOnCompleteListener {
+            if (!it.isSuccessful) {
+                Log.e("REMOTE SET USER", it.exception.toString())
+            }
+        }
+        return Completable.complete()
+    }
+
+    fun getUserById(id: String): Flowable<ApiResponse<UserModel>> {
+        val result = PublishSubject.create<ApiResponse<UserModel>>()
+
+        firebaseFirestore.collection("users").document(id).get()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val data = it.result
+                    result.onNext(
+                        if (data != null) ApiResponse.Success(
+                            UserModel(
+                                id = data.id,
+                                email = data.get("email").toString(),
+                                name = data.get("name").toString(),
+                                hobby = data.get("hobby").toString(),
+                                balance = data.get("balance") as Int,
+                            )
+                        ) else ApiResponse.Empty
+                    )
+                } else {
+                    result.onNext(ApiResponse.Error(it.exception?.message.toString()))
+                    Log.e("REMOTE SIGN IN", it.exception.toString())
                 }
             }
         return result.toFlowable(BackpressureStrategy.BUFFER)
---
+++ app/src/main/java/com/faroh/airplaneandroid/core/domain/model/UserJson.kt
@@ -0,0 +1,8 @@
+package com.faroh.airplaneandroid.core.domain.model
+
+data class UserJson(
+    val email: String?,
+    val name: String?,
+    val hobby: String?,
+    val balance: Int?,
+)
